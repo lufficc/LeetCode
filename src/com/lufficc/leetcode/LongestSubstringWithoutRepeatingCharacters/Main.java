57fe42be7282c1a6314f26e4aa0748eb5693ba98
lufficc <luffy.lcc@gmail.com>
2016-10-23 21:47:47 +0800

    commit

--- src/com/lufficc/leetcode/LongestSubstringWithoutRepeatingCharacters/Main.java
+++ src/com/lufficc/leetcode/LongestSubstringWithoutRepeatingCharacters/Main.java
@@ -4,8 +4,19 @@ package com.lufficc.leetcode.LongestSubstringWithoutRepeatingCharacters;
  * Created by lufficc on 2016/10/23.
  */
 public class Main {
-    public int lengthOfLongestSubstring(String s) {
+    public static void main(String[] args) {
+        System.out.print(new Main().lengthOfLongestSubstring("aab"));
+    }
 
-        return 0;
+    public int lengthOfLongestSubstring(String s) {
+        int begin = 0, max = 0, i = 0;
+        boolean[] set = new boolean[1 << 8];
+        for (char c : s.toCharArray()) {
+            while (set[c]) set[s.charAt(begin++)] = false;
+            set[c] = true;
+            max = Math.max(max, ++i - begin);
+        }
+
+        return max;
     }
 }
